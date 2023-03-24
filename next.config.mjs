419fa1f21e59793c2682c3e8b6dd781973ec1085
inovue <inovue3@gmail.com>
2023-03-24 17:11:30 +0900

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+
+import withPlugins from 'next-compose-plugins'
+import nextra from 'nextra'
+import withExportImages from 'next-export-optimize-images'
+
+
+const withNextra = nextra({
+  theme: 'nextra-theme-blog',
+  themeConfig: './theme.config.jsx',
+  staticImage: true,
+  defaultShowCopyCode: true,
+  readingTime: true
+})
+
+const nextConfig = {
+  reactStrictMode: true,
+  /*
+  images: {
+    loader: 'akamai',
+    path: '/'
+  }
+  */
+}
+
+export default withPlugins([ withNextra, withExportImages ], nextConfig)
