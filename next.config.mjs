b4c3385ebddef5d997e82533b03f57f4c357446a
inovue <inovue3@gmail.com>
2023-03-28 10:21:00 +0900

    dev commit

--- next.config.mjs
+++ next.config.mjs
@@ -17,8 +17,6 @@ const nextConfig = {
   basePath: process.env.GITHUB_REPO ? `/${process.env.GITHUB_REPO}`  : '',
   //assetPrefix: process.env.GITHUB_REPO ? `/${process.env.GITHUB_REPO}`  : '',
   images:{
-    //unoptimized: true,
-    
     loader: "custom",
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
